44c7ca826bb28db86a9349dcae2372e4a28e2ff6
Suresh <115452537+sureshsuriya@users.noreply.github.com>
2026-08-02 18:26:22 +0530

    chore: complete phase 1 - project initialization and structure

---
+++ backend/src/test/java/com/brocrewz/backend/BrocrewzBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brocrewz.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrocrewzBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
